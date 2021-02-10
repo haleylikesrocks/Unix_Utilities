221759d96018e8221c7d352c9fe53c75600cd128
Haley Rairez <ramirezhaley@gmail.com>
2021-02-10 11:15:20 -0600

    working on getting files with multiple line that match correct. does work with one file with multiple line and uses getc instead of getline.

--- wzip/wzip.c
+++ wzip/wzip.c
@@ -8,11 +8,9 @@ int main(int argc, char **argv)
   FILE *fp;
   int file_counter = 1;
   char letter;
+  char next_letter;
   int letter_count = 0;
-  int total_count = 0;
-  char *line_buffer = NULL;
-  size_t line_buffer_size =0;
-  ssize_t line_size = 0;
+
 
   if(argc == 1){
     printf("wzip: file1 [file2 ...]\n");
@@ -27,43 +25,24 @@ int main(int argc, char **argv)
       printf("wzip: file1 [file2 ...]\n");
       exit(1);
     }
-    
-    // get line from file -try changing to fgets
-    line_size = getline(&line_buffer, &line_buffer_size, fp);
-    /// read the next file
 
-    // loop through text and compress
-    letter = line_buffer[total_count];
-
-    while(total_count < line_size){
-      total_count++;
+    letter = getc(fp);
+    do {
+      // letter = getc(fp);
       letter_count++;
-      
-
-      if(line_size == total_count){
-        line_size = getline(&line_buffer, &line_buffer_size, fp);
-        total_count = 0;
-        if(line_size == -1){
-          break;
-        }
-      }
-
-      if(letter != line_buffer[total_count]) {
-        // fwrite(&letter_count, 4, 1, stdout);
-        // fwrite(&letter, 1, 1, stdout);
+      next_letter = getc(fp);
+      if(next_letter != letter && next_letter != EOF){
         printf("%d", letter_count);
         printf("%c", letter);
-
-        letter = line_buffer[total_count];
-        letter_count = 1;
+        letter_count = 0;
+        letter = next_letter;
       }
-    }
+    } while(next_letter != EOF);
+  
     // total_count = 0;
     fclose(fp);
     file_counter++;
   }
-  // fwrite(&letter_count, 4, 1, stdout);
-  // fwrite(&letter, 1, 1, stdout);
   printf("%d", letter_count);
   printf("%c", letter);
   exit(0);
